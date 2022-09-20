4b82d34982ff5361cb400a16cf0ba385c4f72ccb
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-09-20 17:45:37 +0300

    delete

--- greeter.js
+++
@@ -1,6 +0,0 @@
-function greeter(name) {
-    console.log("hello ".concat(name));
-}
-var user = 'Ayhan';
-greeter(user);
-console.log('helloworld');
--- type-assertions.js
+++
@@ -1,4 +0,0 @@
-var message;
-message = 'Hello world';
-var count = message.length;
-var length = message.length;
