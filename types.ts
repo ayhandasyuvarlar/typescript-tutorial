13d9639a0c3b25d5790c281cfa4a281a6b36b97d
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-09-20 16:48:31 +0300

    data types

--- types.ts
+++ types.ts
@@ -1,2 +1,30 @@
 let number = 4
 number = `a`
+
+let a: number
+a = 4
+a = 'hello'
+
+let b
+
+b = 4
+b = 'hello'
+b = true
+
+let c: number[] = [1, 2, 3, 4, 5, 6]
+let d: Array<number> = [1, 2, 3, 4, 5, 6]
+let e: [number, string, boolean] = [1, 'hello', true]
+
+let krediPayment = 0
+const havalePayment = 1
+let eftPayment = 2
+
+enum Payment {
+  kredi = 0,
+  havale = 1,
+  eft = 2,
+}
+
+let kredi = Payment.kredi
+let havale = Payment.havale
+let eft = Payment.eft
